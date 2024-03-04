8dece14b2099a7b189a2abe84b2a1dfe431c29de
YJKZHUZHU <18470186610@163.com>
2024-03-04 15:48:39 +0800

    fix: 校验

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
+  console.log(ddddd)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div>豆芽音乐</div>
+      <div>豆芽音乐</div>
     </main>
-  );
+  )
 }
